e88c23b72350411d7eb0409db491d903548a8e9a
Alex D <broscutamaker@gmail.com>
2017-07-04 23:50:43 +0300

    start_qemu.sh: enable monitor socket; pass command-line arguments to QEMU
    idea:
    https://www.wagner.pp.ru/fossil/vws/wiki?name=QEMU+monitor+on+unix+domain+socket

--- start_qemu.sh
+++ start_qemu.sh
@@ -13,4 +13,10 @@ qemu-system-aarch64 -M arm-generic-fdt-7series \
   -kernel zImage \
   -drive if=sd,format=raw,index=0,file=beta_20170109.dd \
   -boot mode=5 \
-  -append "root=/dev/mmcblk0p2 ro rootwait rootfstype=ext4"
+  -append "root=/dev/mmcblk0p2 ro rootwait rootfstype=ext4" \
+  -chardev socket,server,nowait,path=qemu.monitor,id=monsock \
+  -mon chardev=monsock,mode=readline \
+  $*
+
+# for interactive QEMU monitor, in a second terminal:
+# socat - UNIX-CONNECT:qemu.monitor
