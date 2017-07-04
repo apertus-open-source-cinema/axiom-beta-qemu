da71cf82886986e599a2e7200a48aca7592ef2a3
Alex D <broscutamaker@gmail.com>
2017-07-04 23:47:28 +0300

    start_qemu.sh: nicer formatting of long command

--- start_qemu.sh
+++ start_qemu.sh
@@ -4,4 +4,13 @@ cd AXIOM_BETA_VM
 
 echo "	****TURNING ON QEMU****"
 
-qemu-system-aarch64 -M arm-generic-fdt-7series -machine linux=on -m 1024 -serial /dev/null -serial mon:stdio -nographic -dtb devicetree.dtb -kernel zImage -drive if=sd,format=raw,index=0,file=beta_20170109.dd -boot mode=5 -append "root=/dev/mmcblk0p2 ro rootwait rootfstype=ext4"
+qemu-system-aarch64 -M arm-generic-fdt-7series \
+  -machine linux=on -m 1024 \
+  -serial /dev/null \
+  -serial mon:stdio \
+  -nographic \
+  -dtb devicetree.dtb \
+  -kernel zImage \
+  -drive if=sd,format=raw,index=0,file=beta_20170109.dd \
+  -boot mode=5 \
+  -append "root=/dev/mmcblk0p2 ro rootwait rootfstype=ext4"
